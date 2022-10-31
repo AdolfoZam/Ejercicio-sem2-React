f422e05b5a910918fe3639ef7248712f644cc6f9
adolfo zamora pescador <adolfozamora1005@gmail.com>
2022-10-30 23:38:24 -0500

    consumo de APIS-Weather

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import reactLogo from './assets/react.svg'
+import './App.css'
+import axios from 'axios'
+import ApiClima from './componentes/ApiClima'
+
+function App() {
+
+  
+  
+
+  return (
+    <div className="App">
+      <ApiClima/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/componentes/ApiClima.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import axios from 'axios';
+import { useState } from 'react';
+import { useEffect } from 'react';
+
+const ApiClima = () => {
+
+    const [weather,setWeather] = useState({})
+
+    useEffect (() => {
+                     
+            const success = pos =>{
+                const lat = pos.coords.latitude 
+                const lon = pos.coords.longitude 
+                axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=5a53f2c68115980668e6a817011c345a`)
+                     .then(res => setWeather(res.data))
+            }
+             navigator.geolocation.getCurrentPosition(success);
+    }, [])
+            const centigrade=weather.main?.temp - 273.15
+            const fahrenheit = (centigrade*1.8)+32
+            const [isCentigrade,setIsCentigrade] = useState(true)
+            const convert= () =>{
+                // alert(fahrenheit)
+                setIsCentigrade(!isCentigrade)
+            }
+
+            console.log(weather)
+
+    return (
+        <div className='app'>
+            <div className="con-weather">
+                <h1>Weather App</h1>
+                <p>{weather.sys?.country} {"-"} {weather.name}</p>
+                <div className="contain-general">
+                    <div className="container-img">
+
+                        <img src={` http://openweathermap.org/img/wn/${weather.weather?.[0].icon}@2x.png`} alt="" />
+                        <p> <b>{isCentigrade ? "Grade Celsius" : "Fahrenheit"} {""}</b>
+                            {isCentigrade ? centigrade : fahrenheit}
+                        </p>
+                    </div>
+                    <div className="container-date">
+                        <p><b>Tem-Max:</b>
+                            {weather.main?.temp_max - 273.15} <br />
+                            <b>Tem-Min:</b>
+                            {weather.main?.temp_min - 273.15} <br />
+                            <b>Wind-Speed:</b>
+                            {weather.wind?.speed} <br />
+                            <b>Description:</b>
+                            {weather.weather?.[0].main} <br />
+                            <b>humidity:</b>
+                            {weather.main?.humidity} <br />
+                        </p>
+                    </div>
+                </div>
+                <button on onClick={convert}>{isCentigrade ? "Fahrenheit" : "Centigrade"}</button>
+            </div>
+            
+        </div>
+    );
+};
+
+export default ApiClima;
